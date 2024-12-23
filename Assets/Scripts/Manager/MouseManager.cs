3f0fe59916ae0d2503822b443b54c72c27dc0bd0
Yranib <a729121423@gmail.com>
2024-12-23 23:06:30 +0800

    鼠标管理 A106 主动技能样例

---
+++ Assets/Scripts/Manager/MouseManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    
+    //是否在释放技能
+    public bool isReleaseSkill=false;
+    //是否可以选择敌人卡牌
+    public bool isSelectEnemyCard=false;
+    //被选择的卡牌
+    public List<Card> selectedCards;
+    
+    //选择卡牌数量
+    public int selectCardNum=0;
+    
+    private static MouseManager _instance;
+    public static MouseManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<MouseManager>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject("GamePointBoard");
+                    _instance = obj.AddComponent<MouseManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SkillExecute(List<Card> cards)
+    {
+        Debug.Log("SkillExecute");
+        
+        DynamicEventBus.Publish("SkillExecute", cards);
+        isReleaseSkill = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Skill/SkillEffect/A106Effect.cs
+++ Assets/Scripts/Skill/SkillEffect/A106Effect.cs
@@ -18,9 +18,21 @@ public class A106Effect : BaseEffect
 
     public override void Execute()
     {
-
+        DynamicEventBus.Subscribe<List<Card>>("SkillExecute", SkillExecute);
+        
+        
+        
         // SkillPool.Instance.ReturnSkillFromPlayerSkill("A106");
         // SkillPool.Instance.RemovePlayerSkillByID("A106");
+        
+        
+        //End Execute
+        DynamicEventBus.Subscribe<List<Card>>("SkillExecute", SkillExecute);
+    }
+
+    public void SkillExecute(List<Card> cards)
+    {
+        AllyPoint.Instance.holder.cards.Add(cards[0]);
     }
     
     public override void ImmediateTrigger()
