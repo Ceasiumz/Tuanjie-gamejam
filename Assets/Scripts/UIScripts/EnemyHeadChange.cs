a31e0cd9191c373affa4f00e42f9691d84645302
劣仁假 <a729121423@gmail.com>
2024-12-27 04:10:06 +0800

    敌人姓名板

--- Assets/Scripts/UIScripts/EnemyHeadChange.cs
+++ Assets/Scripts/UIScripts/EnemyHeadChange.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +20,8 @@ public class EnemyHeadChange : MonoBehaviour
     public Sprite enemyHead_6;
     public Sprite enemyHead_dying_6;
 
+    public GameObject EnemyNameBar;
+    
     public EnemyAchive enemy;
 
 
@@ -34,24 +37,31 @@ public class EnemyHeadChange : MonoBehaviour
         {
             case 0:
                 GetComponent<Image>().sprite = enemyHead_1;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "无名之辈";
                 break;
             case 1:
                 GetComponent<Image>().sprite = enemyHead_2;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "克劳德";
                 break;
             case 2:
                 GetComponent<Image>().sprite = enemyHead_3;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "一无所有";
                 break;
             case 3:
                 GetComponent<Image>().sprite = enemyHead_4;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "锟斤铐烫烫烫";
                 break;
             case 4:
                 GetComponent<Image>().sprite = enemyHead_5;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "喧嚣的羔羊";
                 break;
             case 5:
                 GetComponent<Image>().sprite = enemyHead_6;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "塞琳";
                 break;
             default:
                 GetComponent<Image>().sprite = enemyHead_1;
+                EnemyNameBar.GetComponentInChildren<TextMeshProUGUI>().text = "DefaultName";
                 break;
         }
     }
